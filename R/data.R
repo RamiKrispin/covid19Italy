195b3300606a3c51587345b1cf8eba0e97d11209
RamiKrispin <ramkrisp@umich.edu>
2020-03-17 00:06:53 -0700

    Update the data doc

--- R/data.R
+++ R/data.R
@@ -92,3 +92,42 @@
 
 "italy_region"
 
+
+#' The 2019 Novel Coronavirus COVID-19 (2019-nCoV) Italy Provinces Outbreak Dataset
+#'
+#' @description  daily summary of the Coronavirus (COVID-19) pandemic cases in Italy's provinces
+#'
+#'
+#' @format A data.frame object
+#' @source Presidenza del Consiglio dei Ministri - Dipartimento della Protezione Civile \href{http://www.protezionecivile.it/}{website}
+#' @keywords datasets coronavirus COVID19 Italy
+#' @details The dataset contains the daily summary of the Coronavirus pandemic cases in Italy's regions. The data includes the following fields:
+#'
+#' - date - date of notification (Date class)
+#'
+#' - region_code - region code
+#'
+#' - region_name - region names
+#'
+#' - province_code - province code
+#'
+#' - province_name - province names
+#'
+#' - lat - latitude point
+#'
+#' - long - Longitude point
+#'
+#' - total_positive_cases - total positive cases
+#'
+#' - total_tests - number of tests performed
+#' @examples
+#'
+#' data(italy_province)
+#'
+#' head(italy_province)
+#'
+
+"italy_province"
+
+
+
