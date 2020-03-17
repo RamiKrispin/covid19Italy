fa9f1ac882f6554dba79e96d008323dd0610ef62
RamiKrispin <ramkrisp@umich.edu>
2020-03-16 22:39:05 -0700

    Update the data script

--- data_raw/data_refresh.R
+++ data_raw/data_refresh.R
@@ -1,5 +1,5 @@
 #------------------------------------------------------------------
-# Pulling covid19 data for Italy by province
+# Pulling covid19 data for Italy total
 # Source: Presidenza del Consiglio dei Ministri - Dipartimento della Protezione Civile
 # Website: http://www.protezionecivile.it/
 # Github repo: https://github.com/pcm-dpc
@@ -11,7 +11,7 @@
 
 
 
-italy_total <- covid19italy::italy_total
+italy_current <- covid19italy::italy_total
 
 
 
@@ -34,10 +34,10 @@ italy_total <- read.csv("https://raw.githubusercontent.com/pcm-dpc/COVID-19/mast
 
 # Testing if there is a change in the data
 
-if(identical(italy_reg_current, italy_total)){
+if(identical(italy_current, italy_total)){
   print("No updates available")
 } else {
-  if(ncol(italy_reg_current) != ncol(italy_total)){
+  if(ncol(italy_current) != ncol(italy_total)){
     stop("The number of columns in the updated data is not maching the one in the current version")
   }
 
@@ -45,7 +45,7 @@ if(identical(italy_reg_current, italy_total)){
     stop("The number of rows in the updated data is lower than the one in the current version")
   }
 
-  if(min(italy_reg_current$date) != min(italy_total$date)){
+  if(min(italy_current$date) != min(italy_total$date)){
     stop("The start date of the new dataset is not match the one in the current version")
   }
 
