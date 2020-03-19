f41e1a65d8dd197f7e8181381115cd38b36931ad
RamiKrispin <ramkrisp@umich.edu>
2020-03-18 22:47:43 -0700

    Update the tests

--- tests/testthat/test_data_valid.R
+++ tests/testthat/test_data_valid.R
@@ -2,5 +2,17 @@ test_that(desc = "Test the dates", {
   expect_equal(base::class(italy_total$date) == "Date", TRUE)
   expect_equal(base::class(italy_region$date) == "Date", TRUE)
   expect_equal(base::class(italy_province$date) == "Date", TRUE)
-  expect_equal(base::min(italy_region$date) == as.Date("2020-02-24"), TRUE)
+  expect_equal(base::min(italy_total$date) == as.Date("2020-02-24"), TRUE)
+})
+
+
+test_that(desc = "Test the structure", {
+  expect_equal(base::ncol(italy_total) == 11, TRUE)
+  expect_equal(base::ncol(italy_region) == 15, TRUE)
+  expect_equal(base::ncol(italy_province) == 10, TRUE)
+
+  expect_equal(base::nrow(italy_total) >= 24, TRUE)
+  expect_equal(base::nrow(italy_region) >= 504, TRUE)
+  expect_equal(base::nrow(italy_province) >= 3072, TRUE)
+
 })
