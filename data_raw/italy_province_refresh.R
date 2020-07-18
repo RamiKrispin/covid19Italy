6f13de04244607278d895c142895248a689f7c18
RamiKrispin <ramkrisp@umich.edu>
2020-07-17 23:38:57 -0700

    update the data

--- data_raw/italy_province_refresh.R
+++ data_raw/italy_province_refresh.R
@@ -33,7 +33,7 @@ italy_province <- df1 %>% dplyr::left_join(df2,
   dplyr::select(-total_cases_lag) %>%
   dplyr::mutate(new_cases = ifelse(is.na(new_cases), total_cases, new_cases)) %>%
   dplyr::mutate(province_spatial = province_name)
-
+italy_province$province_name <-  ifelse(italy_province$province_name == "Forlì-Cesena", "Forli-Cesena", italy_province$province_name)
 italy_province$province_spatial <- ifelse(italy_province$province_spatial == "Aosta", "Aoste", italy_province$province_spatial)
 italy_province$province_spatial <- ifelse(italy_province$province_spatial == "Bolzano", "Bozen", italy_province$province_spatial)
 italy_province$province_spatial <- ifelse(italy_province$province_spatial == "Crotone", "Crotene", italy_province$province_spatial)
@@ -44,6 +44,7 @@ italy_province$province_spatial <- ifelse(italy_province$province_spatial == "Re
 italy_province$province_spatial <- ifelse(italy_province$province_spatial == "Reggio nell'Emilia", "Reggio Emilia", italy_province$province_spatial)
 italy_province$province_spatial <- ifelse(italy_province$province_spatial == "Torino", "Turin", italy_province$province_spatial)
 italy_province$province_spatial <- ifelse(italy_province$province_spatial == "Barletta-Andria-Trani", "Barletta-Andria Trani", italy_province$province_spatial)
+italy_province$province_spatial <- ifelse(italy_province$province_spatial == "Forlì-Cesena", "Forli-Cesena", italy_province$province_spatial)
 
 
 
