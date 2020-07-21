9a84900c53e28f5d2582aeaba4cf6fc1406f6479
RamiKrispin <ramkrisp@umich.edu>
2020-07-21 00:36:33 -0700

    adding missing dependencies

--- docker/packages/install_geo_packages.R
+++ docker/packages/install_geo_packages.R
@@ -3,6 +3,8 @@ pkg_list <- c("mapview", "sf", "leafpop", "rgeos", "rnaturalearth")
 
 install.packages(pkgs = pkg_list, repos = "https://cran.rstudio.com/")
 
+install.packages("rnaturalearthhires", repos = "http://packages.ropensci.org", type = "source")
+
 for(i in pkg_list){
 
   if(!i %in% rownames(installed.packages())){
