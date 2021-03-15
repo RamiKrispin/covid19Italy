cdc587f7c5779a6d92630883b116c6cb903a0c3e
RamiKrispin <ramkrisp@umich.edu>
2021-03-14 22:15:10 -0700

    updated the total dataset

--- data_raw/italy_total_refresh.R
+++ data_raw/italy_total_refresh.R
@@ -7,27 +7,33 @@ print(branch)
 if(branch != "master" && branch != "dev"){
   stop("The branch argument is missing")
 }
-italy_total <- read.csv("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv",
-                        stringsAsFactors = FALSE) %>%
+italy_total <- readr::read_csv("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv") %>%
   stats::setNames(c("date_temp", "state",
                     "hospitalized_with_symptoms","intensive_care",
                     "total_hospitalized", "home_confinement",
                     "cumulative_positive_cases", "daily_positive_cases",
                     "daily_cases", "recovered",
+
                     "death", "positive_clinical_activity",
                     "positive_surveys_tests",
                     "cumulative_cases",
                     "total_tests", "total_people_tested",
-                    "notes_it")) %>%
+                    "notes_it", "new_intensive_care",
+                    "test_notes", "cases_notes",
+                    "total_positive_molecular_test",
+                    "total_positive_rapid_antigen_test",
+                    "molecular_test",
+                    "rapid_antigen_test"
+                    )) %>%
   dplyr::mutate(date = lubridate::ymd(substr(date_temp, 1, 10))) %>%
-  dplyr::select(-date_temp, -state, -notes_it,  -daily_cases) %>%
+  dplyr::select(-date_temp, -state, -notes_it,  -daily_cases, -test_notes, -cases_notes) %>%
   dplyr::select(date, dplyr::everything()) %>%
   dplyr::arrange(date)
 
 
-if(ncol(italy_total) != 14){
+if(ncol(italy_total) != 19){
   stop("The number of columns is invalid")
-} else if(nrow(italy_total) < 140){
+} else if(nrow(italy_total) < 380){
   stop("The number of raws does not match the minimum number of rows")
 } else if(min(italy_total$date) != as.Date("2020-02-24")){
   stop("The starting date is invalid")
@@ -36,9 +42,9 @@ if(ncol(italy_total) != 14){
 italy_total_csv <- read.csv(sprintf("https://raw.githubusercontent.com/RamiKrispin/covid19Italy/%s/csv/italy_total.csv", branch) , stringsAsFactors = FALSE) %>%
   dplyr::mutate(date = as.Date(date))
 
-if(ncol(italy_total_csv) != 14){
+if(ncol(italy_total_csv) != 19){
   stop("The number of columns is invalid")
-} else if(nrow(italy_total_csv) < 140){
+} else if(nrow(italy_total_csv) < 380){
   stop("The number of raws does not match the minimum number of rows")
 } else if(min(italy_total_csv$date) != as.Date("2020-02-24")){
   stop("The starting date is invalid")
