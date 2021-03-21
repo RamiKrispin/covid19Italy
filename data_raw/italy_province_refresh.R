79c56511e2a8f2821faec4b659223d24e5b1bbc2
RamiKrispin <ramkrisp@umich.edu>
2021-03-21 09:01:58 -0700

    update the province format

--- data_raw/italy_province_refresh.R
+++ data_raw/italy_province_refresh.R
@@ -3,10 +3,10 @@ update_italy_province <- function(branch = "master"){
 `%>%` <- magrittr::`%>%`
 
 df1 <- readr::read_csv("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province.csv",
-                       col_types = cols(note = col_character(),
-                                        codice_nuts_1 = col_character(),
-                                        codice_nuts_2 = col_character(),
-                                        codice_nuts_3 = col_character()),
+                       col_types = readr::cols(note = readr::col_character(),
+                                        codice_nuts_1 = readr::col_character(),
+                                        codice_nuts_2 = readr::col_character(),
+                                        codice_nuts_3 = readr::col_character()),
                        na = "empty") %>%
   stats::setNames(c("date_temp", "state", "region_code", "region_name", "province_code",
                     "province_name", "province_abb", "lat", "long", "total_cases",
@@ -18,6 +18,8 @@ df1 <- readr::read_csv("https://raw.githubusercontent.com/pcm-dpc/COVID-19/maste
 
 
 head(df1)
+
+tail(df1)
 
 
 
@@ -53,24 +55,32 @@ italy_province$province_spatial <- ifelse(italy_province$province_spatial == "Ba
 italy_province$province_spatial <- ifelse(italy_province$province_spatial == "Forlì-Cesena", "Forli-Cesena", italy_province$province_spatial)
 
 
+italy_province <- italy_province %>%
+  dplyr::select(date, region_name, region_code,
+                province_name, province_spatial, province_abb, province_code,
+                lat, long, new_cases, total_cases,
+                nuts_code_1, nuts_code_2, nuts_code_3)
 
 if(ncol(italy_province) != 14){
   stop("The number of columns is invalid")
-} else if(nrow(italy_province) < 54800){
+} else if(nrow(italy_province) < 55500){
   stop("The number of raws does not match the minimum number of rows")
 } else if(min(italy_province$date) != as.Date("2020-02-24")){
   stop("The starting date is invalid")
 }
 
+
+
+
 italy_province_csv <- readr::read_csv(sprintf("https://raw.githubusercontent.com/RamiKrispin/covid19italy/%s/csv/italy_province.csv", branch),
-                                      col_types = cols(nuts_code_1 = col_character(),
-                                                       nuts_code_2 = col_character(),
-                                                       nuts_code_3 = col_character()),
+                                      col_types = readr::cols(nuts_code_1 = readr::col_character(),
+                                                       nuts_code_2 = readr::col_character(),
+                                                       nuts_code_3 = readr::col_character()),
                                       na = "empty")
 
 if(ncol(italy_province_csv) != 14){
   stop("The number of columns is invalid")
-} else if(nrow(italy_province_csv)< 54800){
+} else if(nrow(italy_province_csv)< 55500){
   stop("The number of raws does not match the minimum number of rows")
 } else if(min(italy_province_csv$date) != as.Date("2020-02-24")){
   stop("The starting date is invalid")
