c09c3b492a7dc3810ff5071190699755fa2dbcbb
RamiKrispin <ramkrisp@umich.edu>
2021-07-04 00:20:57 -0700

    update the changes in the dataset

--- tests/testthat/test_data_valid.R
+++ tests/testthat/test_data_valid.R
@@ -10,7 +10,7 @@ test_that(desc = "Test the dates", {
 test_that(desc = "Test the structure", {
 
   expect_equal(base::ncol(italy_total) == 19, TRUE)
-  expect_equal(base::ncol(italy_region) == 19, TRUE)
+  expect_equal(base::ncol(italy_region) == 26, TRUE)
   expect_equal(base::ncol(italy_province) == 14, TRUE)
 
   expect_equal(base::nrow(italy_total) >= 380, TRUE)
